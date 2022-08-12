e0d6c946276ea5c9449a863276e7639edc15cb4c
Berutorion <eagel163013@gmail.com>
2022-08-12 16:35:23 +0800

    feat: add login page

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { engine } = require("express-handlebars");
+const User = require("./models/user");
 const app = express();
 
 const port = 3000;
@@ -8,6 +9,8 @@ const port = 3000;
 app.engine(".hbs", engine({ extname: ".hbs" }));
 app.set("view engine", ".hbs");
 app.set("views", "./views");
+//body-parser
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(port, () => {
   console.log(`Express server is working on http://localhost:${port}`);
@@ -15,4 +18,15 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
   res.render("login");
+});
+
+app.post("/", (req, res) => {
+  const { email, password } = req.body;
+  const user = User.find((user) => email === user.email);
+
+  user
+    ? user.password === password
+      ? res.render("profile", { firstName: user.firstName })
+      : res.render("login", { message: "email of password is wrong" })
+    : res.render("login", { message: "email of password is wrong" });
 });
